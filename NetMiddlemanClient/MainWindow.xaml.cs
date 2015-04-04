44c2eb8f76499ae0141b0bf9659655bb6890f94a
moonrailgun <moonrailgun@gmail.com>
2015-04-04 22:07:48 +0800

    TCP协议基本
    ADD 服务端客户端软件之间相互的TCP连接
    PS：需要解决Close_Wait状态

--- NetMiddlemanClient/MainWindow.xaml.cs
+++ NetMiddlemanClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
 
 namespace NetMiddlemanClient
 {
@@ -20,9 +22,69 @@ namespace NetMiddlemanClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string NMipaddress;
+        public int NMport;
+        public int Listenport;
+
+        private TcpClient NMClient;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.StopListenButton.IsEnabled = false;
+        }
+
+        private void OnListenClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NMipaddress = this.NMipaddressTextBox.Text;
+                NMport = Convert.ToInt32(this.NMportTextBox.Text);
+                Listenport = Convert.ToInt32(this.ListenPortTextBox.Text);
+
+                if (NMport > 65535 || Listenport > 65535 || NMport <= 0 || Listenport <= 0)
+                {
+                    this.StateLabel.Content = "端口号范围在0~65535";
+                    return;
+                }
+
+                NMConnectStart();
+            }
+            catch (FormatException)
+            {
+                this.StateLabel.Content = "请输入合法的数字作为端口";
+            }
+        }
+
+        private void NMConnectStart()
+        {
+            try
+            {
+                if (NMClient == null)
+                {
+                    NMClient = new TcpClient();
+                }
+
+                this.StartListenButton.IsEnabled = false;
+                this.StopListenButton.IsEnabled = true;
+
+                this.StateLabel.Content = "正在连接到远程服务器";
+                NMClient.Connect(NMipaddress, NMport);
+                this.StateLabel.Content = "连接成功";
+            }
+            catch(Exception ex)
+            {
+                this.StateLabel.Content = "无法初始化TCP连接";
+            }
+        }
+
+        private void OnListenStop(object sender, RoutedEventArgs e)
+        {
+            NMClient.Close();
+            NMClient = null;
+            this.StartListenButton.IsEnabled = true;
+            this.StopListenButton.IsEnabled = false;
+            this.StateLabel.Content = "已关闭监听端口";
         }
     }
 }
--- NetMiddlemanServer/MainWindow.xaml.cs
+++ NetMiddlemanServer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
 
 namespace NetMiddlemanServer
 {
@@ -20,9 +22,65 @@ namespace NetMiddlemanServer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int NMport;
+        public int Listenport;
+
+        private TcpListener NMListener;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.StopListenButton.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// 按下开始监听按钮
+        /// </summary>
+        private void OnListenClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NMport = Convert.ToInt32(this.NMportTextBox.Text);
+                Listenport = Convert.ToInt32(this.ListenPortTextBox.Text);
+
+                if (NMport > 65535 || Listenport > 65535 || NMport <= 0 || Listenport <= 0)
+                {
+                    this.StateLabel.Content = "端口号范围在0~65535";
+                    return;
+                }
+
+                NMListenStart();
+            }
+            catch (FormatException fe)
+            {
+                this.StateLabel.Content = "请输入合法的数字作为端口";
+            }
+
+        }
+
+        private void NMListenStart()
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            try
+            {
+                NMListener = new TcpListener(ipAddress, NMport);
+                NMListener.Start(1);
+                this.StartListenButton.IsEnabled = false;
+                this.StopListenButton.IsEnabled = true;
+                this.StateLabel.Content = "等待接入连接";
+            }
+            catch (Exception ex)
+            {
+                this.StateLabel.Content = "无法初始化TCP连接";
+            }
+        }
+
+        private void OnStopListen(object sender, RoutedEventArgs e)
+        {
+            NMListener.Stop();
+            this.StartListenButton.IsEnabled = true;
+            this.StopListenButton.IsEnabled = false;
+            this.StateLabel.Content = "已关闭监听端口";
         }
     }
 }
